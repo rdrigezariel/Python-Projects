b944f1db210acc94b2ec5bb9562429384c08acae
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2021-04-28 15:42:10 -0400

    Upload inheritance script file

---
+++ inheritance_assignment.py
@@ -0,0 +1,12 @@
+class Pet:
+    name = "No Name Provided"
+    age = 0
+
+class Dog(Pet):
+    noise_level = 'High'
+    breed = 'Poodle'
+
+class Fish(Pet):
+    breed = 'Tuna'
+    scales = True
+    
