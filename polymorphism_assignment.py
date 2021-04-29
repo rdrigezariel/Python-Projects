eb33d65b6d802d88fe30e524b492308f5579f2db
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2021-04-29 17:58:18 -0400

    Update script with comments

--- polymorphism_assignment.py
+++ polymorphism_assignment.py
@@ -14,12 +14,14 @@ class Person:
         else:
             print("The password or email is incorrect.")
 
-# Child Class Teacher
+# Child Class Teacher (inherits the Person class)
 class Teacher(Person):
     hourly_pay = 15.00
     department = "Science"
     pin_code = "12345"
 
+    # This is the same method in the Person class.
+    # The difference is that, instead of using entry_password, we're using entry_pin.
     def login(self):
         print("\nTeacher Login:")
         entry_name = input("\tEnter your name: ")
@@ -31,22 +33,25 @@ class Teacher(Person):
         else:
             print("The password or email is incorrect.")
 
-# Child Class Student
+# Child Class Student (inherits the Person class)
 class Student(Person):
     grade = 12
     gpa = 3.5
-    pin_code = "54321"
 
+    # This is the same method in the Person class.
+    # The difference is that, instead of using entry_pin as the Teacher class, we use the password inherited in the Person class.
     def login(self):
         print("\nStudent Login:")
         entry_name = input("\tEnter your name: ")
         entry_email = input("\tEnter your email: ")
-        entry_pin_code = input("\tEnter your pin: ")
+        entry_password = input("\tEnter your pin: ")
 
-        if (entry_email == self.email and entry_pin_code == self.pin_code):
+        if (entry_email == self.email and entry_password == self.password):
             print("Welcome back, {}! Your GPA is currently: {}.".format(entry_name, self.gpa))
         else:
             print("The password or email is incorrect.")
+
+#The following code invokes the methods inside each child class for Teacher and Student.
 
 teacher = Teacher()
 teacher.login()
